b0079d804c734a8effe5f8cc86945ca94930df7d
Erdem Çavdar <erdemcav@hotmail.com>
2024-03-17 19:18:59 +0300

    Add Payment Microservice.

---
+++ Services/Payment/MB.Services.Payment/Controllers/PaymentsController.cs
@@ -0,0 +1,18 @@
+﻿using MB.Shared.ControllerBases;
+using MB.Shared.Dtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MB.Services.Payment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentsController : CustomBaseController
+    {
+        [HttpPost]
+        public IActionResult ReceivePayment()
+        {
+            return CreateActionResultInstance(Response<NoContent>.Success(200));
+        }
+    }
+}
